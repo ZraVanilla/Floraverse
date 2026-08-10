e6bf3deff67d6ce0b7e616df7d2c11d72f67415c
Flintzon <flintzon89@gmail.com>
2026-08-10 11:31:24 +0700

    Add files via upload

---
+++ FloraVerse/js/tailwind-config.js
@@ -0,0 +1,2 @@
+window.tailwind = window.tailwind || {};
+window.tailwind.config = { theme: { extend: { colors: { sky: '#3a9bdc', leaf: '#297a3a', mint: '#93cf5c', soil: '#71452b', sun: '#ffc943', berry: '#e84b4f', bloom: '#c36bd7' }, fontFamily: { pixel: ['"Press Start 2P"', 'monospace'], retro: ['VT323', 'monospace'] } } } };
